7ca271bae5df69f56e9f34644784a47c3091deee
AdoyoClifford <adoyoclifford@gmail.com>
2023-03-01 22:55:35 +0300

    initial commit

---
+++ app/src/main/java/com/adoyo/bluetoothscan/data/chat/BluetoothReceiver.kt
@@ -0,0 +1,27 @@
+package com.adoyo.bluetoothscan.data.chat
+
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+
+class BluetoothReceiver(
+    private val onDeviceFound: (BluetoothDevice) -> Unit
+) : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when (intent?.action) {
+            BluetoothDevice.ACTION_FOUND -> {
+                val device = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    intent.getParcelableExtra(
+                        BluetoothDevice.EXTRA_DEVICE,
+                        BluetoothDevice::class.java
+                    )
+                } else {
+                    intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                }
+                device?.let(onDeviceFound)
+            }
+        }
+    }
+}
